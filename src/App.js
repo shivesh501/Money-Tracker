114bf2a7cfbe4d61788f354234d7d1a8db16851e
Shivesh K <shivkrish501@gmail.com>
2025-11-18 02:00:31 +0530

    built the structure of the money tracker app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <main><h1>Rs. 400<span>.00</span></h1>
+     
+     <form>
+      <div className="basic">
+      <input type="text" placeholder={'+3000 groceries'}/>
+      <input type="datetime-local" />
+      </div>
+      <div className="description">
+      <input type="text" placeholder={'description'}/>
+      </div>
+      <button type="submit">Add new transaction</button>
+     </form>
+
+     <div className="transactions">
+      <div className="transaction">
+        <div className="left">
+          <div className="name">Grocery</div>
+          <div className="description">Weekly grocery haul</div>
+        </div>
+        <div className="right">
+          <div className="price red">-Rs. 3000</div>
+          <div className="datetime">2025-07-16 14:14 </div>
+        </div>
+      </div>
+
+      <div className="transaction">
+        <div className="left">
+          <div className="name">Stipend</div>
+          <div className="description">Monthly stipend</div>
+        </div>
+        <div className="right">
+          <div className="price green">+Rs. 133000</div>
+          <div className="datetime">2025-07-16 14:14 </div>
+        </div>
+      </div>
+     </div>
+     </main>
   );
 }
 
